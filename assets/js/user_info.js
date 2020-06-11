a7dfa052f91152523bc77bff905affd60f6c64ed
zzh <1033409817@qq.com>
2020-06-11 11:08:47 +0800

    完成个人中心的开发

---
+++ assets/js/user_info.js
@@ -0,0 +1,51 @@
+$(function () {
+    var form = layui.form
+    var layer = layui.layer
+    form.verify({
+        nickname: function (value) {
+            if (value.length > 6) {
+                return '昵称长度必须在 1 ~ 6 个字符之间'
+            }
+        }
+    })
+    intUserInfo()
+    // 获取用户的基本信息
+    function intUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败')
+                }
+                // console.log(res)
+                // 调用form.val()快速为表单赋值
+                form.val('formUserInfo', res.data)
+            }
+        })
+    }
+    //重置表单数据
+    $('#btnReset').on('click',function(e){
+        //阻止默认重置行为
+        e.preventDefault()
+        intUserInfo()
+    })
+    //监听表单的提交事件
+     $('.layui-form').on('submit',function(e){
+        // 阻止表单默认提交行为
+         e.preventDefault()
+         $.ajax({
+            method:'POST',
+            url:'/my/userinfo',
+            data:$(this).serialize(),
+            success: function(res){
+                if(res.status !== 0){
+                    return layer.msg('更新用户信息失败')
+                }
+            layer.msg('更新用户信息成功')
+            //调用父页面中的方法重新渲染用户的头像和用户的信息
+            window.parent.getUserInfo()
+            }
+         })
+     })
+})
